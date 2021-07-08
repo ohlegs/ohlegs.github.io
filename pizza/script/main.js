1b3c4b0cd5894cfeba7e274d133597e603d331ef
ohlegs <58605548+ohlegs@users.noreply.github.com>
2021-07-09 00:15:08 +0300

    Add files via upload

--- pizza/script/main.js
+++ pizza/script/main.js
@@ -2,15 +2,17 @@ let blockMenu = document.querySelectorAll(".dropdown");
 // Array.isArray(blockMenu);
 
 for (key of blockMenu) {
+  // console.log(key.children);
   key.addEventListener("click", function (event) {
-    let dropdownDiv = document.getElementById("dropdown_close");
-    if (dropdownDiv.classList == "dropdown_close") {
-      dropdownDiv.classList.remove("dropdown_close");
-      dropdownDiv.classList.add("dropdown_open");
-    } else if (dropdownDiv.classList == "dropdown_open") {
-      dropdownDiv.classList.remove("dropdown_open");
-      dropdownDiv.classList.add("dropdown_close");
-    }
+    let dropdownDiv = event.target.children;
+      
+    if (dropdownDiv[0].classList == "dropdown_close") {
+        dropdownDiv[0].classList.remove("dropdown_close");
+        dropdownDiv[0].classList.add("dropdown_open");
+      } else if (dropdownDiv[0].classList == "dropdown_open") {
+        dropdownDiv[0].classList.remove("dropdown_open");
+        dropdownDiv[0].classList.add("dropdown_close");
+      }
   });
 }
 
